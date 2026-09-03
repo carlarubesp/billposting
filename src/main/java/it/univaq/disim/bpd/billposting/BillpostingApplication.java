f25fcacb15793401a8f0f0014c260766372084e6
carla <carlarubesp@gmail.com>
2026-09-03 21:26:24 +0200

    chore: bootstrap Spring Boot and create the project skeleton

---
+++ src/main/java/it/univaq/disim/bpd/billposting/BillpostingApplication.java
@@ -0,0 +1,11 @@
+package it.univaq.disim.bpd.billposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillpostingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BillpostingApplication.class, args);
+    }
+}
